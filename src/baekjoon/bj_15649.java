fabf394a8aa1902a29bb02cef737fe3a012976f5
CJ.KIM <kcj1284@gmail.com>
2022-07-19 21:53:20 +0900

    0719

---
+++ src/baekjoon/bj_15649.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj_15649 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int [] arr = new int [m];
+		int i=0;
+		while(i<n) {
+			for(int j =1;j<=m;j++) {
+				Math.abs(-5);
+			}
+			
+		}
+	}
+
+}
